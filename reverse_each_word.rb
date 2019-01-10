9ffe36b0d459e738b17639260a8fdcf177f86b98
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-10 16:27:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(string)
   temp_arr = Array.new
   string.split.each{ |word| temp_arr << word.reverse}
   temp_arr.join(' ')
-
+  string.split.collect{ |word| word.reverse}.join(' ')
 end
